2eb1af08b8d920b36722b153d3cb638a13cb75f6
Daisuke Yamamoto <dqa03151@gmail.com>
2013-07-30 11:49:44 +0900

    use ratchet toggle control, add glyphicons icons

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,9 +1,16 @@
 $(function() {
 
+    "use strict";
+    
     var timer;
 
     // konashiとつながったら
     k.ready(function() {
+
+        // 接続中のkonashiの名前を取得
+        k.peripheralName(function(name) {
+            $("#target").text(name);
+        });
 
         // IO設定
         k.pinMode(k.LED2, k.OUTPUT);
@@ -19,31 +26,37 @@ $(function() {
         k.find();
     });
 
-    // Startボタン
-    $("#start_btn").click(function() {
-        // 100msごとに実行
-        var i = 0;
-        var dir = true;
-        timer = setInterval(function() {
-            // LED点灯
-            k.digitalWriteAll(1 << (i + k.LED2));
-
-            if (dir) {
-                i++;
-            } else {
-                i--;
-            }
-            if (i <= 0) dir = true;
-            if (i >= 3) dir = false;
-        }, 100);
+    // LED - 個別
+    $(".toggle").on("toggle", function(e) {
+        var pin = $(e.currentTarget).data("pin");
+        var value = $(e.currentTarget).hasClass("active");
+        k.digitalWrite(pin, (value)? k.HIGH : k.LOW);
     });
 
-    // Stopボタン
-    $("#stop_btn").click(function() {
-        // タイマ停止
-        clearInterval(timer);
-        // LEDすべてOFF
-        k.digitalWriteAll(0);
+    // LED - KnightRider
+    $("#toggleKR").on("toggle", function(e) {
+        var value = $(e.currentTarget).hasClass("active");
+        if (value === true) {
+            // 100msごとに実行
+            var i = 0;
+            var dir = true;
+            timer = setInterval(function() {
+                // LED点灯
+                k.digitalWriteAll(1 << (i + k.LED2));
+                if (dir) {
+                    i++;
+                } else {
+                    i--;
+                }
+                if (i <= 0) dir = true;
+                if (i >= 3) dir = false;
+            }, 100);
+        } else {
+            // タイマ停止
+            clearInterval(timer);
+            // LEDすべてOFF
+            k.digitalWriteAll(0);
+        }
     });
 
 });
